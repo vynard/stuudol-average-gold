7be642d5bcaf8dc192cadf56ca75ae55060cdb30
Brenner Little <Brenner Little@LAPTOP-D5JU4MGC>
2022-03-26 00:50:20 -0700

    fixed db

--- web test.py
+++ web test.py
@@ -1,30 +1,44 @@
 from flask import *
-from flask_mysqldb import MySQL
-from click import *
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+
+
+
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'userdata'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+db=SQLAlchemy(app)
 
-mysql = MySQL(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(20), unique=True, nullable=False)
+    lastname = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phonenumber = db.Column(db.String(120), unique=True, nullable=False)
+    course = db.Column(db.String(20), unique=True, nullable=False)
 
 
+    def __repr__(self):
+
+
+        return f"User({self.firstname}',{self.lastname}','{self.username}',{self.pnumber}', '{self.email}',{self.course}')"
+
+db.create_all()
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
         submission = request.form
-        firstName = submission['fname']
-        lastName = submission['lname']
-        userName = submission['uname']
+        fn = submission['fname']
+        ln= submission['lname']
+        un = submission['uname']
         eMail = submission['email']
         pNumber = submission['pnumber']
         firstClass = submission['class1']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(first_name, last_name, user_name, email, phone_number) VALUES (%s, %s, %s, %s, %s)", (firstName, lastName, userName, eMail, pNumber))
-        mysql.connection.commit()
-        cur.close()
+        cuser= User( username=un,lastname=ln,firstname=fn,email= eMail,phonenumber=pNumber,course=firstClass)
+        db.session.add(cuser)
+        db.session.commit()
+
         return 'success'
     return render_template('backbone.html')
 
